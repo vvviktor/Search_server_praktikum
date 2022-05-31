ba5e4eb41dfcb8edef44276ba4cb73a00605e12b
Viktor Kustov <kustovvictor@gmail.com>
2022-05-31 05:51:04 +0300

    refactored, remove_duplicates added, all tests

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -16,16 +16,16 @@ void RemoveDuplicates(SearchServer& search_server) {
         }
     }
 
-    map<int, set<string>> ids_to_words2 = ids_to_words;
-
     for (const auto& [id, words]: ids_to_words) {
-        for (const auto& [id2, words2]: ids_to_words2) {
-            if (id2 == id) {
+        if (ids_to_remove.count(id)) {
+            continue;
+        }
+        for (const auto& [id2, words2]: ids_to_words) {
+            if (id2 == id || ids_to_remove.count(id2)) {
                 continue;
             }
             if (words2 == words) {
                 ids_to_remove.insert(id2);
-                ids_to_words.erase(id2);
                 cout << "Found duplicate document id "s << id2 << endl;
             }
         }
