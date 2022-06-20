3d6bbaa9de4529187e1a6ca1589c7b22d117187a
Viktor Kustov <kustovvictor@gmail.com>
2022-06-20 16:51:52 +0300

    RemoveDocument ExecutionPolicy added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,94 @@
-#include "process_queries.h"
+#include "search_server.h"
+
+#include <execution>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "log_duration.h"
+
+using namespace std;
+
+string GenerateWord(mt19937& generator, int max_length) {
+    const int length = uniform_int_distribution(1, max_length)(generator);
+    string word;
+    word.reserve(length);
+    for (int i = 0; i < length; ++i) {
+        word.push_back(uniform_int_distribution<int>('a', 'z')(generator));
+    }
+    return word;
+}
+
+vector<string> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
+    vector<string> words;
+    words.reserve(word_count);
+    for (int i = 0; i < word_count; ++i) {
+        words.push_back(GenerateWord(generator, max_length));
+    }
+    sort(words.begin(), words.end());
+    words.erase(unique(words.begin(), words.end()), words.end());
+    return words;
+}
+
+string GenerateQuery(mt19937& generator, const vector<string>& dictionary, int max_word_count) {
+    const int word_count = uniform_int_distribution(1, max_word_count)(generator);
+    string query;
+    for (int i = 0; i < word_count; ++i) {
+        if (!query.empty()) {
+            query.push_back(' ');
+        }
+        query += dictionary[uniform_int_distribution<int>(0, dictionary.size() - 1)(generator)];
+    }
+    return query;
+}
+
+vector<string> GenerateQueries(mt19937& generator, const vector<string>& dictionary, int query_count, int max_word_count) {
+    vector<string> queries;
+    queries.reserve(query_count);
+    for (int i = 0; i < query_count; ++i) {
+        queries.push_back(GenerateQuery(generator, dictionary, max_word_count));
+    }
+    return queries;
+}
+
+template <typename ExecutionPolicy>
+void Test(string_view mark, SearchServer search_server, ExecutionPolicy&& policy) {
+    LOG_DURATION(mark);
+    const int document_count = search_server.GetDocumentCount();
+    for (int id = 0; id < document_count; ++id) {
+        search_server.RemoveDocument(policy, id);
+    }
+    cout << search_server.GetDocumentCount() << endl;
+}
+
+#define TEST(mode) Test(#mode, search_server, execution::mode)
+
+int main() {
+    mt19937 generator;
+
+    const auto dictionary = GenerateDictionary(generator, 10'000, 25);
+    const auto documents = GenerateQueries(generator, dictionary, 10'000, 100);
+
+    {
+        SearchServer search_server(dictionary[0]);
+        for (size_t i = 0; i < documents.size(); ++i) {
+            search_server.AddDocument(i, documents[i], DocumentStatus::ACTUAL, {1, 2, 3});
+        }
+
+        TEST(seq);
+    }
+    {
+        SearchServer search_server(dictionary[0]);
+        for (size_t i = 0; i < documents.size(); ++i) {
+            search_server.AddDocument(i, documents[i], DocumentStatus::ACTUAL, {1, 2, 3});
+        }
+
+        TEST(par);
+    }
+}
+
+/*
 #include "search_server.h"
 
 #include <iostream>
@@ -23,14 +113,24 @@ int main() {
         search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
     }
 
-    const vector<string> queries = {
-            "nasty rat -not"s,
-            "not very funny nasty pet"s,
-            "curly hair"s
+    const string query = "curly and funny"s;
+
+    auto report = [&search_server, &query] {
+        cout << search_server.GetDocumentCount() << " documents total, "s
+             << search_server.FindTopDocuments(query).size() << " documents for query ["s << query << "]"s << endl;
     };
-    for (const Document& document : ProcessQueriesJoined(search_server, queries)) {
-        cout << "Document "s << document.id << " matched with relevance "s << document.relevance << endl;
-    }
+
+    report();
+    // однопоточная версия
+    search_server.RemoveDocument(5);
+    report();
+    // однопоточная версия
+    search_server.RemoveDocument(execution::seq, 1);
+    report();
+    // многопоточная версия
+    search_server.RemoveDocument(execution::par, 2);
+    report();
 
     return 0;
 }
+*/
--- search_server.h
+++ search_server.h
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <stdexcept>
 #include <cmath>
+#include <execution>
 #include "document.h"
 #include "string_processing.h"
 #include "log_duration.h"
@@ -25,6 +26,9 @@ public:
 
     void AddDocument(int document_id, const std::string& document, DocumentStatus status,
                      const std::vector<int>& ratings);
+
+    template<class ExecutionPolicy>
+    void RemoveDocument(ExecutionPolicy&& policy, int document_id);
 
     void RemoveDocument(int document_id);
 
@@ -118,8 +122,37 @@ SearchServer::FindTopDocuments(const std::string& raw_query, DocumentPredicate d
     return matched_documents;
 }
 
+template<class ExecutionPolicy>
+void SearchServer::RemoveDocument(ExecutionPolicy&& policy, int document_id) {
+    bool existence = std::any_of(policy, document_ids_.begin(), document_ids_.end(), [document_id](const auto& elem) {
+        return elem == document_id;
+    });
+
+    if (!existence) {
+        using namespace std::literals::string_literals;
+        throw std::invalid_argument("Attempt to remove non-existing ID."s);
+    }
+
+    std::vector<std::string> words_to_process;
+    words_to_process.reserve(document_to_word_freqs_.at(document_id).size());
+
+    for (const auto& [word, freq]: document_to_word_freqs_.at(document_id)) {
+        words_to_process.emplace_back(word);
+    }
+
+    std::for_each(policy, words_to_process.begin(),
+                  words_to_process.end(), [this, document_id](auto& word) {
+                word_to_document_freqs_.at(word).erase(document_id);
+            });
+
+    document_to_word_freqs_.erase(document_id);
+    documents_.erase(document_id);
+    document_ids_.erase(document_id);
+}
+
 template<typename DocumentPredicate>
-std::vector<Document> SearchServer::FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const {
+std::vector<Document>
+SearchServer::FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const {
     std::map<int, double> document_to_relevance;
     for (const std::string& word: query.plus_words) {
         if (word_to_document_freqs_.count(word) == 0) {
