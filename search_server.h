490e6d4eac84a255b475b9fcbafa521b048a3c7e
Viktor Kustov <kustovvictor@gmail.com>
2022-06-29 04:22:25 +0300

    String_view impl

--- search_server.h
+++ search_server.h
@@ -168,6 +168,7 @@ template<class ExecutionPolicy>
 std::tuple<std::vector<std::string_view>, DocumentStatus>
 SearchServer::MatchDocument(ExecutionPolicy&& policy, std::string_view raw_query, int document_id) const {
     assert(std::is_execution_policy_v<std::decay_t<ExecutionPolicy>>);
+
     if constexpr(std::is_same_v<std::decay_t<ExecutionPolicy>,  // 26-й тест в тренажере успешно выполняется только с этим условием.
             std::execution::sequenced_policy>) {
         return MatchDocument(raw_query, document_id);
